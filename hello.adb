16aa2ae9dd9134dd445c2f35d29fde3bedb07288
BKambic <98051852+BKambic@users.noreply.github.com>
2022-01-19 14:06:13 -0600

    Update hello.adb
    Sending an update with a goodbye loop.

--- hello.adb
+++ hello.adb
@@ -3,5 +3,16 @@ with Ada.Text_IO;
 procedure Hello is
     use Ada.Text_IO;
 begin
-    Put_Line ("Hello Ada!");
+
+    for I in 1..6 loop
+    
+        if I mod 2  = 0 then
+            Put_Line ("Goodbye Ada!"); -- Sends a goodbye messgae on all even numbered loops
+    
+        else
+           Put_Line ("Hello Ada!"); -- Sends a Hello messgae on all odd numbered loops
+        end if;
+        
+    end loop;
+
 end Hello;
